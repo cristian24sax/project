3e7cea5a305538d9000920a992250cc54c0943f1
jesus24 <cristian.stone9@gmail.com>
2021-05-21 00:03:35 -0500

    corrigiendo los nombres y puesto

--- webPersonal/webPersonal/settings.py
+++ webPersonal/webPersonal/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ckeditor',
+    'ckeditor', 
     'mainapp',
     'portafolio',
 ]
